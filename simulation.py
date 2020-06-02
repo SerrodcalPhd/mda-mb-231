2276432a36fe1b66f7daf0e06b81a22003280c37
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2020-06-02 16:56:51 +0200

    some changes in live

--- simulation.py
+++ simulation.py
@@ -4,9 +4,19 @@ import time
 from random import randint, random
 
 iterations = 100
-migration = 0.25 # Depending on IR
-mitosis = 0.1 # Depending on IR
-f = 0.2
+migration = 0.5 #0.35 # Depending on IR
+mitosis = 0.25 #0.01 # Depending on IR
+f = 0.1
+
+# 10 gray -> diferencia de mitosis
+# Opciones:
+# 1) Área biología celular -> cuantificación (menos valor)
+# 2) Inteligencia artifial -> ???
+
+# RPE1:
+
+# Empuje
+# Modificación en funcion de la radiación (neo -> agente imita la radiación en terminos de daño en el ADN)
 
 # Pygame: esquina superior izquierda el origen, tupla (columna, fila)
 pygame.init()
@@ -26,14 +36,21 @@ dimCH = height / rows # Alto -> filas
 # Estado de las celdas: Viva = 1; Muerta = 0
 gameState = np.zeros((rows, cols)) # (filas, columnas)
 
+cont_cells = 0
+
 # Rellenamos ambos margenes
 for y in range(0, rows):
     for x in range(0, 11):
         gameState[y, x] = 1
+        cont_cells += 1
 
 for y in range(0, rows):
     for x in range(34, 45):
         gameState[y, x] = 1
+        cont_cells += 1
+
+gameState[10,10] = 2
+
 #gameState[15,15] = 1
 
 def neighborhood(position, grid, rows, cols):
@@ -58,10 +75,16 @@ def neighborhood(position, grid, rows, cols):
     return free
 
 # Control de la ejecucion del juego
-pauseExect = False
+pauseExect = True
 
 # Contador de iteraciones
 cont = 0
+
+# Contadores
+cont_movements = 0
+cont_mitosis = 0
+
+start = time.time()
 
 # Bucle de ejecucion
 while True and cont < iterations:
@@ -83,15 +106,21 @@ while True and cont < iterations:
         for y in range(cols): # Columnas
 
             if not pauseExect:
-                if gameState[x, y] == 1:
+                if gameState[x, y] > 0:
                     neigh = neighborhood((x, y), newGameState, rows, cols)
                     if neigh:
                         next = neigh[randint(0, len(neigh)-1)]
                         if random() < migration: # Migracion
-                            newGameState[x, y] = 0
-                            newGameState[next] = 1
+                            if newGameState[x, y] == 2:
+                                newGameState[x, y] = 0
+                                newGameState[next] = 2
+                            else:
+                                newGameState[x, y] = 0
+                                newGameState[next] = 1
+                            cont_movements += 1
                         elif random() < mitosis: # Division celular
-                            newGameState[next] = 1
+                            newGameState[next] = 3 # Indicamos 2 para saber que es una celula nueva, por mitosis
+                            cont_mitosis += 1
 
     # Pintado
     for x in range(rows): # Filas
@@ -109,12 +138,18 @@ while True and cont < iterations:
                          ((y+0.1) * dimCW, (x+0.9) * dimCH)]
 
             # Y dibujamos la celda para cada par de x e y
-            if newGameState[x, y] == 0:
+            if newGameState[x, y] == 0: # Celda sin celula
                 pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
-            else:
-                #pygame.draw.polygon(screen, (255, 255, 255), poly, 0)
+            elif newGameState[x, y] == 1: # Celda con celula por movimiento
                 pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
                 pygame.draw.polygon(screen, (255, 255, 255), thickness, 0)
+            elif newGameState[x, y] == 2:
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+                pygame.draw.polygon(screen, (0, 255, 0), thickness, 0)
+            else: # Celda con celula por mitosis
+                newGameState[x, y] = 1 # Lo devolvemos a blanco
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+                pygame.draw.polygon(screen, (255, 0, 0), thickness, 0)
 
     # Actualizamos el estado del juego
     gameState = np.copy(newGameState)
@@ -125,5 +160,11 @@ while True and cont < iterations:
     # Actualizamos contador
     cont += 1
 
-print("END")
+end = time.time()
+
+print("Total simulation's time: " + str(end - start))
+print("Total cells: " + str(cont_cells + cont_mitosis))
+print("Total movements: " + str(cont_movements))
+print("Total mitosis: " + str(cont_mitosis))
+
 time.sleep(9999)
